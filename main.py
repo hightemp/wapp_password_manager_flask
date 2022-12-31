839d6f137bd22e96cc557c97bd1b6c6a337a5734
Панов Антон <hightemp.unknown@gmail.com>
2022-12-31 10:52:05 +0300

    Sat Dec 31 10:52:05 AM MSK 2022 update

--- main.py
+++ main.py
@@ -508,3 +508,9 @@ def index():
         sCategoryFilter=sCategoryFilter,
         sAccountFilter=sAccountFilter
     )
+
+def run():
+    app.run(host='0.0.0.0')
+
+if __name__ == "__main__":
+    run()
